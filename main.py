74f943b2c81316f3765dd9761aaa8f264692eb61
esfalsa <pronoun.nationstates@gmail.com>
2023-04-01 00:19:55 -0700

    Fix practice detag flagging

--- main.py
+++ main.py
@@ -39,6 +39,112 @@ update_length = int(region_nodes[-1].find("LASTUPDATE").text) - update_start
 
 log(f"Found update length: {update_length} seconds.", level="info")
 
+wfe_criteria = {
+    "[region]the brotherhood of malice[/region]": "BoM",
+    "[region]the black hawks[/region]": "TBH",
+    "[region]valle de arena[/region]": "Osiris",
+    "[region]lily[/region]": "Lily",
+    "[region]lone wolves united[/region]": "LWU",
+    "[region]ijaka[.region]": "Ijaka",
+    "[region]the militia[/region]": None,
+    "[region]kantrias[/region]": "Kantrias",
+    "[region]sparkalia[/region]": "Sparkalia",
+    "region=the_brotherhood_of_malice": "BoM",
+    "region=the_black_hawks": "TBH",
+    "region=valle_de_arena": "Osiris",
+    "region=lily": "Lily",
+    "region=ijaka": "Ijaka",
+    "region=the_militia": None,
+    "region=kantrias": "Kantrias",
+    "region=sparkalia": "Sparkalia",
+    "lilystates.proboards.com": "Lily",
+    "region=lone_wolves_united": "LWU",
+    "www.forum.the-black-hawks.org": "TBH",
+    "forums.europeians.com/index.php?forums/office-of-naval-recruitment.59364": "ERN",
+    "www.nationstates.net/page=dispatch/id=1344417": "TWP",
+    "forum.theeastpacific.com/executive-application-thread-t16445.html": "EPSA",
+    "forum.theeastpacific.com/t/apply-to-join-the-eastern-pacific-sovereign-army/16275": "EPSA",
+    "tag run": None,
+}
+
+roman_numeral_regex = "m{0,4}(cm|cd|d?c{0,3})(xc|xl|l?x{0,3})(ix|iv|v?i{0,3})"
+# https://stackoverflow.com/a/267405
+
+offices_criteria = {
+    "raider unity": None,
+    "thorn1000": None,
+    "join tbh": "TBH",
+    "join %%lily%%": "Lily",
+    "lily": "Lily",
+    "the funny": None,
+    "empress wasc": None,
+    "ern": "ERN",
+    "twpirate": "TWP",
+    "twpirates": "TWP",
+    "kanye omari west": None,
+    "aga gang": "EPSA",
+    "epsa": "EPSA",
+    "hellfire hawk": None,
+    "kantrian overseer": "Kantrias",
+    "thaempirial army": None,
+}
+
+ro_criteria = [
+    "guy_\d+",
+    "rc_cola_\d+",
+    "ijaka(\d|10)",
+    "taiko_no_tatsujin_\d+",
+    "bobberino\d+",
+    "\d+(rd|th|nd|st)_catgirl_division",
+    "switz_got_lazy_\d+",
+    "switz_\d+",
+    "sweeze_\d+",
+    "tls_\d+",
+    "flap_flap_boom_\d+",
+    "liliarchy_ancillary_\d+",
+    "lucklife_\d+",
+    "lurklife_\d+",
+    "wednesday_\d+",
+    "thursday_\d+",
+    "thorn\d+",
+    "wascoitan_?\d+",
+    "pineapple_on_pizza_is_good_\d+",
+    "foxes_\d+",
+    "oversized_operativez_\d+",
+    "cretanja_garrison_\d+",
+    f"legionnaries_{roman_numeral_regex}",
+    f"legionnary_{roman_numeral_regex}",
+    "souls\d+",
+    "rb\d+",
+    "remus_\d+",
+    f"remus_{roman_numeral_regex}",
+    "yor_\d+",
+    "punch_from_mark_lee_\d+",
+    "upc_is_not_fast_\d+",
+    "beans_on_toast_\d+",
+    "bigred\d+",
+    "terberrinse_\d+",
+    "flame_of_chaos_\d+",
+    "narioni_\d+",
+    "jyezet_fighter_\d+",
+    f"{roman_numeral_regex}_proleterska_vazduhoplovna_brigada",
+    "fedaykin_\d+",
+]
+
+embassies_criteria = {
+    "The Black Hawks": "TBH",
+    "The Brotherhood of Malice": "BoM",
+    "Valle de Arena": "Osiris",
+    "Red Front": "TCB",
+    "Plum Island": "Lily",
+    "Kingdom of Australia": "EoGB",
+    "Pasridi Confederacy": "EPSA",
+    "Ijaka": "Ijaka",
+    "Islarabia": None,
+    "The Militia": None,
+    "Agheasma": "Sparkalia",
+}
+
 
 def find_issues(region):
     if (
@@ -61,34 +167,6 @@ def find_issues(region):
 
     wfe = (region.find("FACTBOOK").text or "").lower()
 
-    wfe_criteria = {
-        "[region]the brotherhood of malice[/region]": "BoM",
-        "[region]the black hawks[/region]": "TBH",
-        "[region]valle de arena[/region]": "Osiris",
-        "[region]lily[/region]": "Lily",
-        "[region]lone wolves united[/region]": "LWU",
-        "[region]ijaka[.region]": "Ijaka",
-        "[region]the militia[/region]": None,
-        "[region]kantrias[/region]": "Kantrias",
-        "[region]sparkalia[/region]": "Sparkalia",
-        "region=the_brotherhood_of_malice": "BoM",
-        "region=the_black_hawks": "TBH",
-        "region=valle_de_arena": "Osiris",
-        "region=lily": "Lily",
-        "region=ijaka": "Ijaka",
-        "region=the_militia": None,
-        "region=kantrias": "Kantrias",
-        "region=sparkalia": "Sparkalia",
-        "lilystates.proboards.com": "Lily",
-        "region=lone_wolves_united": "LWU",
-        "www.forum.the-black-hawks.org": "TBH",
-        "forums.europeians.com/index.php?forums/office-of-naval-recruitment.59364": "ERN",
-        "www.nationstates.net/page=dispatch/id=1344417": "TWP",
-        "forum.theeastpacific.com/executive-application-thread-t16445.html": "EPSA",
-        "forum.theeastpacific.com/t/apply-to-join-the-eastern-pacific-sovereign-army/16275": "EPSA",
-        "tag run": None,
-    }
-
     flagged_wfe = [substring for substring in wfe_criteria if substring in wfe]
 
     if flagged_wfe:
@@ -102,31 +180,10 @@ def find_issues(region):
     officer_appointers = [
         (officer.find("BY").text or "").lower() for officer in officers
     ]
+
     officer_offices = [
         (officer.find("OFFICE").text or "").lower() for officer in officers
     ]
-
-    roman_numeral_regex = "m{0,4}(cm|cd|d?c{0,3})(xc|xl|l?x{0,3})(ix|iv|v?i{0,3})"
-    # https://stackoverflow.com/a/267405
-
-    offices_criteria = {
-        "raider unity": None,
-        "thorn1000": None,
-        "join tbh": "TBH",
-        "join %%lily%%": "Lily",
-        "lily": "Lily",
-        "the funny": None,
-        "empress wasc": None,
-        "ern": "ERN",
-        "twpirate": "TWP",
-        "twpirates": "TWP",
-        "kanye omari west": None,
-        "aga gang": "EPSA",
-        "epsa": "EPSA",
-        "hellfire hawk": None,
-        "kantrian overseer": "Kantrias",
-        "thaempirial army": None,
-    }
 
     flagged_offices = [
         office for office in officer_offices if office in offices_criteria
@@ -143,50 +200,7 @@ def find_issues(region):
         )
 
     if "RO" not in issues and any(
-        any(
-            re.fullmatch(regex, officer_appointer)
-            for regex in [
-                "guy_\d+",
-                "rc_cola_\d+",
-                "ijaka(\d|10)",
-                "taiko_no_tatsujin_\d+",
-                "bobberino\d+",
-                "\d+(rd|th|nd|st)_catgirl_division",
-                "switz_got_lazy_\d+",
-                "switz_\d+",
-                "sweeze_\d+",
-                "tls_\d+",
-                "flap_flap_boom_\d+",
-                "liliarchy_ancillary_\d+",
-                "lucklife_\d+",
-                "lurklife_\d+",
-                "wednesday_\d+",
-                "thursday_\d+",
-                "thorn\d+",
-                "wascoitan_?\d+",
-                "pineapple_on_pizza_is_good_\d+",
-                "foxes_\d+",
-                "oversized_operativez_\d+",
-                "cretanja_garrison_\d+",
-                f"legionnaries_{roman_numeral_regex}",
-                f"legionnary_{roman_numeral_regex}",
-                "souls\d+",
-                "rb\d+",
-                "remus_\d+",
-                f"remus_{roman_numeral_regex}",
-                "yor_\d+",
-                "punch_from_mark_lee_\d+",
-                "upc_is_not_fast_\d+",
-                "beans_on_toast_\d+",
-                "bigred\d+",
-                "terberrinse_\d+",
-                "flame_of_chaos_\d+",
-                "narioni_\d+",
-                "jyezet_fighter_\d+",
-                f"{roman_numeral_regex}_proleterska_vazduhoplovna_brigada",
-                "fedaykin_\d+",
-            ]
-        )
+        any(re.fullmatch(regex, officer_appointer) for regex in ro_criteria)
         for officer_appointer in officer_appointers
     ):
         issues.append("RO")
@@ -196,20 +210,6 @@ def find_issues(region):
         for embassy in region.findall(f"./EMBASSIES/EMBASSY")
         if embassy.get("type") not in ["closing", "rejected"]
     ]
-
-    embassies_criteria = {
-        "The Black Hawks": "TBH",
-        "The Brotherhood of Malice": "BoM",
-        "Valle de Arena": "Osiris",
-        "Red Front": "TCB",
-        "Plum Island": "Lily",
-        "Kingdom of Australia": "EoGB",
-        "Pasridi Confederacy": "EPSA",
-        "Ijaka": "Ijaka",
-        "Islarabia": None,
-        "The Militia": None,
-        "Agheasma": "Sparkalia",
-    }
 
     flagged_embassies = [
         embassy for embassy in embassies if embassy in embassies_criteria
@@ -234,12 +234,15 @@ def find_issues(region):
 
     # if issues found, report them
     if issues:
-        # print(issues)
         return {
             "issues": issues,
             "organizations": sorted(organizations),
             "native_embassies": native_embassies,
         }
+
+    del issues
+    del organizations
+    del native_embassies
 
     # otherwise, check for practice detags
 
@@ -247,7 +250,7 @@ def find_issues(region):
     practice_organizations = set()
 
     practice_wfe_criteria = {
-        "Help cleanse this region of the [b][color=C7996C]thiccness[/color][/b], the [b][color=C7996C]gooeyness[/color][/b], and the [b][color=E3CBA1]drip[/color][/b]... [b][color=D6B589]drip[/color][/b]... [b][color=C7996C]drip[/color][/b]... of eggnog by detagging it.": "Eggnog"
+        "Help cleanse this region of the [b][color=C7996C]thiccness[/color][/b], the [b][color=C7996C]gooeyness[/color][/b], and the [b][color=E3CBA1]drip[/color][/b]... [b][color=D6B589]drip[/color][/b]... [b][color=C7996C]drip[/color][/b]... of eggnog by detagging it.".lower(): "Eggnog"
     }
 
     practice_flagged_wfe = [
@@ -271,7 +274,7 @@ def find_issues(region):
     ]
 
     if practice_flagged_offices:
-        issues.append("RO")
+        practice_issues.append("RO")
         practice_organizations.update(
             {
                 practice_offices_criteria[office]
@@ -318,9 +321,15 @@ for region in track(region_nodes, description="Flagging regions…"):
         minor_progress = round(progress * 3600)
         major_progress = round(progress * 5400)
 
+        issues = (
+            region_issues["issues"]
+            if "issues" in region_issues
+            else region_issues["practice_issues"]
+        )
+
         region_data = {
             "Region": f"{name}",
-            "Issues": f"{', '.join(region_issues['issues'])}",
+            "Issues": f"{', '.join(issues)}",
             "Minor": minor_progress,
             "MinorTimestamp": f"{str(timedelta(seconds=minor_progress))}",
             "Major": major_progress,
@@ -336,17 +345,11 @@ for region in track(region_nodes, description="Flagging regions…"):
             if type(value) is str and value[0] in ["=", "+", "-", "@"]:
                 region_data[key] = f"'{value}"
 
-        # if "Region" not in region_data:
-        #     print(region_data, region_issues)
-
         if "issues" in region_issues and region_issues["issues"]:
             regions.append(region_data)
         elif "practice_issues" in region_issues and region_issues["practice_issues"]:
             practice_regions.append(region_data)
 
-    # elif "practice_issues" in region_data and region_data["practice_issues"]:
-
-# print(regions)
 
 today = (datetime.utcfromtimestamp(update_start) - timedelta(1)).strftime("%d %B %Y")
 
